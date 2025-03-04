1a7440720134f79090154af1aace4e5ebf08b13b
pink polkadots <97930483+poinkpolkadots@users.noreply.github.com>
2025-03-04 09:39:35 -0500

    create the client.py script for remaining tables

---
+++ Systemclient.py
@@ -0,0 +1,15 @@
+import requests
+
+# URL of the Flask server
+url = "http://127.0.0.1:5000/get_data"
+
+# Send a GET request
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()  # Convert response to JSON
+    print("Data from System:")
+    for row in data:
+        print(row)
+else:
+    print(f"Error: {response.status_code}")
